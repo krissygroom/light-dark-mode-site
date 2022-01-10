d715c62c8224ad8a58562cb0596950d61b3cdeb9
krissy <krissydgroom@gmail.com>
2022-01-10 12:31:46 -0800

    Completed switch functionality

---
+++ script.js
@@ -0,0 +1,65 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+// Get elements to change to dark mode
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Dark or Light Images
+function imageMode(colorTheme) {
+    image1.src = `img/undraw_proud_coder_${colorTheme}.svg`;
+    image2.src = `img/undraw_conceptual_idea_${colorTheme}.svg`;
+    image3.src = `img/undraw_feeling_proud_${colorTheme}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgba(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgba(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+// Light Mode Styles
+function lightMode() {
+    textBox.style.backgroundColor = 'rgba(0 0 0 / 50%)';
+    nav.style.backgroundColor = 'rgba(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    // if switch is toggled on, then change to dark mode
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        lightMode();
+    }
+}
+
+// Event Listener for toggle switch
+toggleSwitch.addEventListener('change', switchTheme);
+
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    } else {
+        toggleSwitch.checked = false;
+        lightMode();
+    }
+}
